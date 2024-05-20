8b5d17e76563c115e161ae7136775035e410f47b
JerryWang0122 <school832@gmail.com>
2024-05-20 13:41:53 +0800

    feat: BookingDao

---
+++ src/main/java/com/example/demo/dao/BookingDao.java
@@ -0,0 +1,20 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.po.BookingMeetingRoom;
+
+import java.util.List;
+
+public interface BookingDao {
+
+    // 預定會議室
+    Integer addBooking(BookingMeetingRoom bookingMeetingRoom);
+
+    // 取消預訂
+    Integer cancelBooking(Integer bookingId);
+
+    // 查看所有預定
+    List<BookingMeetingRoom> findAllBookingRooms();
+
+    // 修改預約人
+    Integer updateBookingUserId(Integer bookingId, Integer userId);
+}
