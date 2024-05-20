f2ad19e3719da675076b9db01359f4496ca15a78
JerryWang0122 <school832@gmail.com>
2024-05-20 14:31:50 +0800

    feat: row mapper

--- src/main/java/com/example/demo/dao/BookingDaoImpl.java
+++ src/main/java/com/example/demo/dao/BookingDaoImpl.java
@@ -3,11 +3,16 @@ package com.example.demo.dao;
 import com.example.demo.model.dto.BookingMeetingRoomDto;
 import com.example.demo.model.po.BookingMeetingRoom;
 import com.example.demo.model.po.MeetingRoom;
+import com.example.demo.model.po.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,7 +40,48 @@ public class BookingDaoImpl implements BookingDao{
 
     @Override
     public List<BookingMeetingRoomDto> findAllBookingRooms() {
-        return null;
+        String sql = "SELECT " +
+                "b.bookingId, b.roomId, b.userId, b.bookingDate, b.createDate, " +
+                "r.roomId, r.roomName, r.roomSize, " +
+                "u.id, u.name " +
+                "FROM " +
+                "bookingmeetingroom b " +
+                "left join meetingroom r on b.roomId = r.roomId " +
+                "left join user u on b.userId = u.id";
+
+        // 自定義對應邏輯規則
+        RowMapper<BookingMeetingRoomDto> mapper = new RowMapper<>() {
+            @Override
+            public BookingMeetingRoomDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+                // 逐筆逐項將每一個欄位資料抓出
+                Integer bookingId = rs.getInt("bookingId");
+                Integer roomId = rs.getInt("roomId");
+                Integer userId = rs.getInt("userId");
+                String bookingDate = rs.getString("bookingDate");
+                Timestamp createDate = rs.getTimestamp("createDate");
+
+                String roomName = rs.getString("roomName");
+                Integer roomSize = rs.getInt("roomSize");
+
+                String name = rs.getString("name");  // user name
+
+                // 注入資料
+                MeetingRoom meetingRoom = new MeetingRoom(roomId, roomName, roomSize);
+                User user = new User(userId, name);
+
+                BookingMeetingRoomDto dto = new BookingMeetingRoomDto();
+                dto.setBookingId(bookingId);
+                dto.setRoomId(roomId);
+                dto.setUserId(userId);
+                dto.setBookingDate(bookingDate);
+                dto.setCreateDate(createDate);
+
+                dto.setMeetingRoom(meetingRoom);
+                dto.setUser(user);
+                return dto;
+            }
+        };
+        return jdbcTemplate.query(sql, mapper);
     }
 
     @Override
